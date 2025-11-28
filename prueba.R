dfd8aa2c27302870ec08632804260fd805ba9526
Elmer <qselmer@gmail.com>
2025-11-28 08:03:13 -0500

    update 2025

--- prueba.R
+++
@@ -1,9 +0,0 @@
-# cargar datos de peso y longitud de merluza
-merluza <- readRDS("./datos/merluza_WL.rds")
-
-#ver primeros renglones
-head(merluza)
-
-# gráfico tall-peso
-plot(merluza$lonPatron, merluza$peso, pch = 16, col = rgb(1, 0, 0, 0.2), 
-     cex = 0.7, xlab = "Longitud Patrón (mm)", ylab = "Peso (g)")
